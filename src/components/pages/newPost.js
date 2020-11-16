9c664393a596cac4bce07a35a60195757f2cc5f0
TCMooney <tcmooney88@gmail.com>
2020-11-16 15:54:18 -0500

    removed draftjs editor

--- src/components/pages/newPost.js
+++ src/components/pages/newPost.js
@@ -4,7 +4,7 @@ import { BlogContext } from '../contexts/BlogState';
 import InputLabel from '../InputLabel';
 import PageTitle from '../PageTitle';
 
-export default function NewPost(props) {
+export default function NewPost() {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [tags, setTags] = useState('');
@@ -25,21 +25,23 @@ export default function NewPost(props) {
     return (
         <div className='add-blog-form'>
             <PageTitle title='New Blog' />
-            <form className='blog-form' onSubmit={onSubmit}>
-                <div className='new-blog-title blog-form-title'>
-                    <InputLabel labelTitle='Title' />
-                    <input className='blog-title-input' type='text' value={title} onChange={(event) => setTitle(event.target.value)} placholder='Title' />
-                </div>
-                <div className='new-blog-content blog-form-content'>
-                    <InputLabel labelTitle='Content' />
-                    <textarea className='blog-content-input' type='text-area' value={content} onChange={(event) => setContent(event.target.value)} placholder='Content' />
-                </div>
-                <div className='new-blog-tags blog-form-tags'>
-                    <InputLabel labelTitle='Tags' />
-                    <input className='blog-tags-input' type='text' value={tags} onChange={(event) => setTags(event.target.value)} placholder='Tags' />
-                </div>
-                <button className='submit-button'>Add Blog</button>
-            </form>
+            <div className='new-blog-form'>
+                <form className='blog-form' onSubmit={onSubmit}>
+                    <div className='new-blog-title blog-form-title'>
+                        <InputLabel labelTitle='Title' />
+                        <input className='blog-title-input' type='text' value={title} onChange={(event) => setTitle(event.target.value)} placholder='Title' />
+                    </div>
+                    <div className='new-blog-content blog-form-content'>
+                        <InputLabel labelTitle='Content' />
+                        <textarea className='blog-content-input' type='text-area' value={content} onChange={(event) => setContent(event.target.value)} placholder='Content' />
+                    </div>
+                    <div className='new-blog-tags blog-form-tags'>
+                        <InputLabel labelTitle='Tags' />
+                        <input className='blog-tags-input' type='text' value={tags} onChange={(event) => setTags(event.target.value)} placholder='Tags' />
+                    </div>
+                    <button className='submit-button'>Add Blog</button>
+                </form>
+            </div>
         </div>
     )
 }
